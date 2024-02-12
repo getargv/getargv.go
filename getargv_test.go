4ffb2d48cd47203060291852c1840ff99381f90f
Camden Narzt <c.narzt@me.com>
2024-02-12 13:07:05 -0700

    rename some vars for clarity in tests

--- getargv_test.go
+++ getargv_test.go
@@ -41,20 +41,20 @@ func TestAsString(t *testing.T) {
 func TestAsBytes(t *testing.T) {
 	args := os.Args
 	pid := uint(os.Getpid())
-	for _, v := range [2]bool{true, false} {
-		for i := 1; i <= len(args); i++ { // in go 1.22 use: for i := range len(args)+1 {
-			t.Run(fmt.Sprintf("skip=%d, nuls=%t", i, v), func(t *testing.T) {
+	for _, nuls := range [2]bool{true, false} {
+		for skip := 1; skip <= len(args); skip++ { // in go 1.22 use: for i := range len(args)+1 {
+			t.Run(fmt.Sprintf("skip=%d, nuls=%t", skip, nuls), func(t *testing.T) {
 				var sep string
-				if v {
+				if nuls {
 					sep = " "
 				} else {
 					sep = "\x00"
 				}
-				want := []byte(strings.Join(args[i:], sep))
-				if i < len(args) {
+				want := []byte(strings.Join(args[skip:], sep))
+				if skip < len(args) {
 					want = append(want, 0)
 				}
-				actual, err := AsBytes(pid, uint(i), v)
+				actual, err := AsBytes(pid, uint(skip), nuls)
 				if !bytes.Equal(want, actual) || err != nil {
 					t.Fatalf(`%q, %v, should have been: %#q, nil`, actual, err, want)
 				}
@@ -73,20 +73,20 @@ func TestAsStrings(t *testing.T) {
 	}
 }
 
-type failureCase struct {
+type asStringsFailureCase struct {
 	pid uint
 	err error
 }
 
 // TestAsStringsFailure calls getargv.AsStrings with various pids, checking for correct errors.
 func TestAsStringsFailure(t *testing.T) {
-	for _, v := range []failureCase{{0,syscall.EPERM},{99999+1,syscall.ESRCH}} {
+	for _, v := range []asStringsFailureCase{{0, syscall.EPERM}, {99999 + 1, syscall.ESRCH}} {
 		pid := v.pid
 		want := v.err
-		t.Run(fmt.Sprintf("pid = %d, err = %v",pid,want), func(t *testing.T) {
+		t.Run(fmt.Sprintf("pid = %d", pid), func(t *testing.T) {
 		actual, err := AsStrings(pid)
 		if !errors.Is(err, want) {
-			t.Fatalf(`AsStrings(%d) = %q, %+v, should have been: [], %+v`, pid, actual, err, want)
+				t.Fatalf(`%q, %+v, should have been: [], %+v`, actual, err, want)
 		}
 	})
 	}
