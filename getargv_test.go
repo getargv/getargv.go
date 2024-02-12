c1d3e14a8eaf19a3a4ab87e6c860fef40b1beb53
Camden Narzt <c.narzt@me.com>
2024-02-12 13:14:30 -0700

    more tests

--- getargv_test.go
+++ getargv_test.go
@@ -37,6 +37,36 @@ func TestAsString(t *testing.T) {
 	}
 }
 
+type asStringFailureCase struct {
+	pid  pid_t
+	skip uint
+	nuls bool
+	want error
+}
+
+// TestAsStringFailure calls getargv.AsString with pid, nuls, and skip set to cause expected errors
+func TestAsStringFailure(t *testing.T) {
+	this := uint(os.Getpid())
+	num := uint(len(os.Args) + 1)
+
+	for _, v := range []asStringFailureCase{
+		{0, 0, true, syscall.EPERM},
+		{99999 + 1, 0, true, syscall.ESRCH},
+		{this, num, true, syscall.ERANGE},
+	} {
+		pid := v.pid
+		skip := v.skip
+		nuls := v.nuls
+		want := v.want
+		t.Run(fmt.Sprintf("pid=%d, skip=%d, nuls=%t", pid, skip, nuls), func(t *testing.T) {
+			actual, err := AsString(pid, skip, nuls)
+			if !errors.Is(err, want) {
+				t.Fatalf(`%q, %v, should have been: %#q, nil`, actual, err, want)
+			}
+		})
+	}
+}
+
 // TestAsBytes calls getargv.AsBytes with pid, nuls, and skip fully exercised
 func TestAsBytes(t *testing.T) {
 	args := os.Args
@@ -63,6 +93,36 @@ func TestAsBytes(t *testing.T) {
 	}
 }
 
+type asBytesFailureCase struct {
+	pid  pid_t
+	skip uint
+	nuls bool
+	want error
+}
+
+// TestAsBytesFailure calls getargv.AsBytes with pid, nuls, and skip set to cause expected errors
+func TestAsBytesFailure(t *testing.T) {
+	this := uint(os.Getpid())
+	num := uint(len(os.Args) + 1)
+
+	for _, v := range []asBytesFailureCase{
+		{0, 0, true, syscall.EPERM},
+		{99999 + 1, 0, true, syscall.ESRCH},
+		{this, num, true, syscall.ERANGE},
+	} {
+		pid := v.pid
+		skip := v.skip
+		nuls := v.nuls
+		want := v.want
+		t.Run(fmt.Sprintf("pid=%d, skip=%d, nuls=%t", pid, skip, nuls), func(t *testing.T) {
+			actual, err := AsBytes(pid, skip, nuls)
+			if !errors.Is(err, want) {
+				t.Fatalf(`%q, %v, should have been: %#q, nil`, actual, err, want)
+			}
+		})
+	}
+}
+
 // TestAsStrings calls getargv.AsStrings with test process' pid, checking for a valid return value.
 func TestAsStrings(t *testing.T) {
 	want := os.Args
@@ -84,10 +144,10 @@ func TestAsStringsFailure(t *testing.T) {
 		pid := v.pid
 		want := v.err
 		t.Run(fmt.Sprintf("pid = %d", pid), func(t *testing.T) {
-		actual, err := AsStrings(pid)
-		if !errors.Is(err, want) {
+			actual, err := AsStrings(pid)
+			if !errors.Is(err, want) {
 				t.Fatalf(`%q, %+v, should have been: [], %+v`, actual, err, want)
-		}
-	})
+			}
+		})
 	}
 }
